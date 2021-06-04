39e79a3385cc06ad1a5d6662b4f2e55d63e63dd4
Matt <mattdawson18@gmail.com>
2021-06-04 15:16:44 +0100

    Extracted validations

--- src/Account.js
+++ src/Account.js
@@ -6,21 +6,29 @@ class Account {
   }
 
   deposit = (amount) => {
-    if( typeof amount !== 'number') {
-      throw new Error('Invalid input type')
-    }
+    this._validate_deposit(amount)
     this.balance += amount
     this.transaction_history.push(new Transaction(new Date(), amount, this.balance))
   }
 
   withdraw = (amount) => {
+    this._validate_withdrawal(amount)
+    this.balance -= amount
+    this.transaction_history.push(new Transaction(new Date(), -amount, this.balance))
+  }
+
+  _validate_deposit = (amount) => {
+    if( typeof amount !== 'number') {
+      throw new Error('Invalid input type')
+    }
+  }
+
+  _validate_withdrawal = (amount) => {
     if( typeof amount !== 'number') {
       throw new Error('Invalid withdrawal request') 
     } else if (amount > this.balance ) {
       throw new Error('Insufficient funds') 
     }
-    this.balance -= amount
-    this.transaction_history.push(new Transaction(new Date(), -amount, this.balance))
   }
 
 }
