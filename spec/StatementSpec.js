dcf350c6837b26c62cb05652610051942f66ce14
Matt <mattdawson18@gmail.com>
2021-06-29 17:12:38 +0100

    Statement can print a statement, but test format looks weird and there is no decimal/ currency notation

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -3,21 +3,31 @@
 describe('statement', function() {
   let statement;
   let transaction;
+  let transaction2;
   let account;
 
   beforeEach(function() {
-    transaction = jasmine.createSpyObj('statement', {}, {time: new Date(), amount: 1000, balance: 1000 });
+    transaction = jasmine.createSpyObj('transaction', {}, {time: new Date(), amount: 1000, balance: 1000 });
+    transaction2 = jasmine.createSpyObj('transaction', {}, {time: new Date(), amount: -500, balance: 500 });
     account = jasmine.createSpyObj('account', {}, { transactionHistory: []})
     account.transactionHistory.push(transaction)
+    account.transactionHistory.push(transaction2)
     statement = new Statement(account)
   })
 
   it('has an mocked account and transaction', function() {
-    console.log(statement)
     expect(statement.account).toBeDefined()
     expect(statement.account.transactionHistory).toBeDefined()
     expect(statement.account.transactionHistory[0]).toBeDefined()
     expect(statement.account.transactionHistory[0].amount).toBe(1000)
+  })
+
+  it('can print a statement', function() {
+    let result = statement.printStatement()
+    let time = new Date()
+    expect(result).toEqual(
+      `Date || Credit || Debit || Balance\n${time.toLocaleDateString()} || || 500 || 500\n${time.toLocaleDateString()} || 1000 || || 1000`  
+    )
   })
 
 })
--- src/Statement.js
+++ src/Statement.js
@@ -2,4 +2,25 @@ class Statement{
   constructor(account){
     this.account = account
   }
+
+  printStatement = () => {
+    let reversedStatement = this.account.transactionHistory.reverse()
+    return "Date || Credit || Debit || Balance\n" +
+    reversedStatement.map((transaction) => 
+      this._formatTransaction(transaction)).join('\n')
+  }
+
+  _formatType = (transaction) => {
+    if(transaction.amount > 0) {
+      return ' || ' + `${Math.abs(transaction.amount)}` + ' || || '
+    } else {
+      return ' || || ' + `${Math.abs(transaction.amount)}` + ' || '
+    }
+  }
+
+  _formatTransaction = (transaction) => {
+    return transaction.time.toLocaleDateString() +
+    this._formatType(transaction) + transaction.balance
+  }
+
 }
